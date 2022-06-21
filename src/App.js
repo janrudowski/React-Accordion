76b4d9ec37c51efc60ac6cbfdf8d59875bcfe55a
Jan Rudowski <j.rudowski@protonmail.com>
2022-06-21 16:31:57 +0200

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AccordionSection from './components/AccordionSection';
+import settingsIcon from './icons/settings.svg';
+import squaresIcon from './icons/four-squares.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='accordion'>
+      <AccordionSection
+        icon={settingsIcon}
+        title='Sort Home By Featured Posts'
+        content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, porro corrupti. Sint cumque tempora minima optio reiciendis sunt. Libero, animi.'
+      />
+      <AccordionSection
+        icon={squaresIcon}
+        title='More Things to Do'
+        content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, porro corrupti. Sint cumque tempora minima optio reiciendis sunt. Libero, animi.'
+      />
     </div>
   );
 }
---
+++ src/components/AccordionSection.jsx
@@ -0,0 +1,36 @@
+import chevron from './../icons/chevron.svg';
+
+import { useState, useRef } from 'react';
+function AccordionSection(props) {
+  const [isActive, setIsActive] = useState(false);
+  const [height, setHeight] = useState('0px');
+  const content = useRef(null);
+  function toggle() {
+    setIsActive((prevState) => !prevState);
+    setHeight(isActive ? `${content.current.scrollHeight}px` : '0px');
+  }
+  return (
+    <div className='accordion-section'>
+      <div className='accordion-btn' onClick={toggle}>
+        <p>
+          <img className='icon' src={props.icon} alt='settings' />
+          {props.title}
+        </p>
+        <img
+          className={`chevron icon ${isActive ? 'chevron--active' : ''}`}
+          src={chevron}
+          alt='chevron'
+        />
+      </div>
+      <div
+        ref={content}
+        style={{ height: `${height}` }}
+        className={`accordion-content ${isActive ? 'accordion--active' : ''}`}
+      >
+        <p dangerouslySetInnerHTML={{ __html: props.content }}></p>
+      </div>
+    </div>
+  );
+}
+
+export default AccordionSection;
