4d7ac3162b60770b1902106b74ef1d4324737156
Jan Rudowski <j.rudowski@protonmail.com>
2022-06-30 20:20:08 +0200

    added readme img

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ function App() {
     <div className='accordion'>
       <AccordionSection
         icon={settingsIcon}
-        title='Sort Home By Featured Posts'
+        title='Lorem Ipsum'
         content='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima non ipsa voluptates. Inventore aliquam facere eligendi? Magni veniam earum dicta impedit nisi a possimus, consectetur praesentium perferendis quos animi ad unde quam ipsa, ducimus similique tempora explicabo nulla laudantium, eaque fugit ipsum. Rerum soluta odio praesentium nulla veritatis eaque totam?'
       />
       <AccordionSection
         icon={squaresIcon}
-        title='More Things to Do'
+        title='Lorem Ipsum'
         content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, porro corrupti. Sint cumque tempora minima optio reiciendis sunt. Libero, animi.'
+      />
+      <AccordionSection
+        icon={squaresIcon}
+        title='Lorem Ipsum'
+        content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi'
       />
     </div>
   );
